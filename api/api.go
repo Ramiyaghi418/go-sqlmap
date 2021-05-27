bdd1c05c637f0d7d1ac862e40aa1a7ec1e8a22a5
EmYiQing <2023503307@qq.com>
2021-05-27 15:21:21 +0800

    v0.0.1

---
+++ api/api.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"github.com/EmYiQing/go-sqlmap/log"
+	"github.com/EmYiQing/go-sqlmap/start"
+	"github.com/EmYiQing/go-sqlmap/util"
+	"os"
+)
+
+type Scanner struct {
+	Options start.Input
+}
+
+func NewScanner(options start.Input) *Scanner {
+	return &Scanner{
+		Options: options,
+	}
+}
+func (w *Scanner) Run() {
+	log.Info("start scan using api...")
+	target := util.CheckUrl(w.Options.Url)
+	log.Info("target is " + target)
+	if !start.DetectAlive(target) {
+		os.Exit(-1)
+	}
+	if start.DetectSafeDogWaf(target) {
+		os.Exit(-1)
+	}
+	start.NewStarter(target, w.Options)
+}
+
+func (w *Scanner) Stop() {
+	log.Info("stop scan using api...")
+	os.Exit(0)
+}
